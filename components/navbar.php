65ad02cb9360800b7fa1c8254c9ef10cb0b89c98
NoFall887 <naufalamiruddin123456@gmail.com>
2022-07-27 12:41:34 +0700

    change navbar to a component change index.html to index.php

---
+++ components/navbar.php
@@ -0,0 +1,42 @@
+<!-- navbar -->
+<nav class="navbar navbarev navbar-expand-sm navbar-light fixed-top" id="navbar">
+  <div class="container">
+    <a class="home-link" href="index.html"><img class="logo" src="assets/LOGO MUN 2022 1.png" alt="">
+      <img class="line" src="assets/stripsy.png" alt="">
+      <p class="identity mr-auto">JAGOMUN</p>
+    </a>
+
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+      <ul class="navbar-nav ml-auto mt-2 mt-lg-0 eventnav">
+        <li class="nav-item active">
+          <a class="nav-link" href="index.html">Home<span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#councils">Councils</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Register
+          </a>
+          <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+            <a class="dropdown-item" href="#single delegates">Single Delegation</a>
+            <a class="dropdown-item" href="#delegation">Delegation</a>
+          </div>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" data-toggle="effect" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#contact">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#store">Store</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<!-- akhirnavbar -->
