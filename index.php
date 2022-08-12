5d7f1d54c7959bdd16b4ba38069c4b42dc27c071
NoFall887 <naufalamiruddin123456@gmail.com>
2022-08-12 18:40:00 +0700

    fix hero background

--- index.php
+++ index.php
@@ -18,76 +18,69 @@
 
   <!-- section -->
   <section class="hero">
-    <img src="assets/gradient.png" alt="">
-    <div class="container">
-      <div class=" row d-flex justify-content-center text-center">
-        <h1 class="mb-4">
-          <div class="col-md-auto">JEMBER ANNUAL GLOBAL</div>
-          <div class="col-md-auto">MODEL UNITED NATIONS</div>
-        </h1>
-        <h2>Sovereignty and Commitment: Overcoming Post-pandemic Obstacles and Challenges </h2>
-      </div>
+    <div class=" row d-flex justify-content-center text-center">
+      <h1 class="mb-4">
+        <div class="col-md-auto">JEMBER ANNUAL GLOBAL</div>
+        <div class="col-md-auto">MODEL UNITED NATIONS</div>
+      </h1>
+      <h2>Sovereignty and Commitment: Overcoming Post-pandemic Obstacles and Challenges </h2>
     </div>
+    <button class="btn d-flex mx-auto main-button" type="button">Register Now</button>
+
+    <div class="row justify-content-md-center mt-5">
+      <div class="text-end col col-lg-3">
+        <h3>November</h3>
+        <p class="mb-1">25 - 27 November 2022</p>
+      </div>
+      <div class="col-md-auto separator">
+      </div>
+      <div class="col col-lg-3">
+        <h3>Jember</h3>
+        <p class="mb-1">Bla Bla Bla Hotel</p>
+      </div>
     </div>
 
-    <div class="container text-center">
-      <button class="btn d-flex mx-auto main-button" type="button">Register Now</button>
-    </div>
-    <div class="desc container">
-      <div class="row justify-content-md-center">
-        <div class="text-end col col-lg-3">
-          <h3>November</h3>
-          <p class="mb-1">25 - 27 November 2022</p>
-        </div>
-        <div class="col-md-auto separator">
-        </div>
-        <div class="col col-lg-3">
-          <h3>Jember</h3>
-          <p class="mb-1">Bla Bla Bla Hotel</p>
-        </div>
-      </div>
-    </div>
   </section>
 
   <section class="documentation">
     <img class="image1" src="assets/image 1.png" alt="">
-    <div class="container">
-        <div class="row d-flex align-items-center">
-          <div class="col-sm-6 text-left">
-            <h3>Previously on JAGOMUN 2021</h3>
-            <p>Jember Annual Global Model United Nations 2021 is the biggest event of the UKM UNEJ MUN Club in the form of a simulation of the United Nations assembly at the international level. JAGOMUN 2021 was held for the first time online in 2021. JAGOMUN 2021 has managed to make a great start by gaining 100+ delegates from 9 countries (Indonesia, Philippines, Zambia, Spain, Russia, India, United Arab Emirates, Cambodia, Saudi Arabia); attracting 11 Chairs from Indonesia, the Philippines, and Nepal; receiving support from the Ministry of Foreign Affairs, United Nations High Commissioner for Refugees in Indonesia, and The Asian Institute of Alternative Dispute Resolutions; and supporting by the Rector of the University of Jember, Governor of East Java Province, Consul-General of the Republic of Indonesia in Frankfurt, and the United Nations Representative for Indonesia.</p>
-          </div>
-          <div class="col-sm-5 offset-sm-1 d-flex align-self-center">
-              <img class="preview" src="assets/vid preview.png" alt="" style="max-width: 100%;  height: auto">
-          </div>
+    <div class="container-fluid px-5">
+      <div class="row d-flex align-items-center">
+        <div class="col-sm-6 text-left">
+          <h3 class="mb-3">Previously on JAGOMUN 2021</h3>
+          <p>Jember Annual Global Model United Nations 2021 is the biggest event of the UKM UNEJ MUN Club in the form of a simulation of the United Nations assembly at the international level. JAGOMUN 2021 was held for the first time online in 2021. JAGOMUN 2021 has managed to make a great start by gaining 100+ delegates from 9 countries (Indonesia, Philippines, Zambia, Spain, Russia, India, United Arab Emirates, Cambodia, Saudi Arabia); attracting 11 Chairs from Indonesia, the Philippines, and Nepal; receiving support from the Ministry of Foreign Affairs, United Nations High Commissioner for Refugees in Indonesia, and The Asian Institute of Alternative Dispute Resolutions; and supporting by the Rector of the University of Jember, Governor of East Java Province, Consul-General of the Republic of Indonesia in Frankfurt, and the United Nations Representative for Indonesia.</p>
+        </div>
+        <div class="col-sm-5 offset-sm-1 d-flex align-self-center">
+          <img class="preview" src="assets/vid preview.png" alt="" style="max-width: 100%;  height: auto">
         </div>
       </div>
+    </div>
 
     <div class="container" style="padding-top: 88px; padding-bottom: 184px;">
       <div class="col-sm-12">
-          <div class="sponsors_slider">
-            <ul>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-              <li><img src="assets/image 2.png" alt="..."></li>
-            </ul>
-          </div>  
-    </div> 
+        <div class="sponsors_slider">
+          <ul>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+            <li><img src="assets/image 2.png" alt="..."></li>
+          </ul>
+        </div>
+      </div>
   </section>
 
   <section class="theme">
 
-      </div>
-  <?php require "./components/script.php" ?>
+    </div>
+    <?php require "./components/script.php" ?>
 </body>
 
 </html>
