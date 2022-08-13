d7a12258e6a16a30b1bfe380d966b2b22f8d25d2
NoFall887 <naufalamiruddin123456@gmail.com>
2022-08-13 12:45:00 +0700

    finished slider added grand theme section (on progress)

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
 <body>
   <?php require "./components/navbar.php"; ?>
 
-  <!-- section -->
+  <!-- hero section -->
   <section class="hero">
     <div class=" row d-flex justify-content-center text-center">
       <h1 class="mb-4">
@@ -39,48 +39,93 @@
         <p class="mb-1">Bla Bla Bla Hotel</p>
       </div>
     </div>
+  </section>
 
+  <!-- countdown section -->
+  <section class="container countdown mt-3">
+    <p class="row">
+      <span class="col">
+        <span id="days">
+          0
+        </span>
+        <span>Days</span>
+      </span>
+      <span class="col">
+        <span id="hours">
+          0
+        </span>
+        <span>Hours</span>
+      </span>
+      <span class="col">
+        <span id="minutes">
+          0
+        </span>
+        <span>Minutes</span>
+      </span>
+      <span class="col">
+        <span id="seconds">
+          0
+        </span>
+        <span>Seconds</span>
+      </span>
+    </p>
   </section>
 
   <section class="documentation">
-    <img class="image1" src="assets/image 1.png" alt="">
-    <div class="container-fluid px-5">
+
+    <div class="container-fluid px-5 documentation-width-limiter">
       <div class="row d-flex align-items-center">
         <div class="col-sm-6 text-left">
           <h3 class="mb-3">Previously on JAGOMUN 2021</h3>
           <p>Jember Annual Global Model United Nations 2021 is the biggest event of the UKM UNEJ MUN Club in the form of a simulation of the United Nations assembly at the international level. JAGOMUN 2021 was held for the first time online in 2021. JAGOMUN 2021 has managed to make a great start by gaining 100+ delegates from 9 countries (Indonesia, Philippines, Zambia, Spain, Russia, India, United Arab Emirates, Cambodia, Saudi Arabia); attracting 11 Chairs from Indonesia, the Philippines, and Nepal; receiving support from the Ministry of Foreign Affairs, United Nations High Commissioner for Refugees in Indonesia, and The Asian Institute of Alternative Dispute Resolutions; and supporting by the Rector of the University of Jember, Governor of East Java Province, Consul-General of the Republic of Indonesia in Frankfurt, and the United Nations Representative for Indonesia.</p>
         </div>
         <div class="col-sm-5 offset-sm-1 d-flex align-self-center">
-          <img class="preview" src="assets/vid preview.png" alt="" style="max-width: 100%;  height: auto">
+          <iframe src="https://drive.google.com/file/d/1lKT3jA2DiMazHA0JloGsfABKdGi45QPd/preview" id="aftermovie" frameborder="0" allow="accelerometer; autoplay; encrypted-media; picture-in-picture" allowfullscreen></iframe>
         </div>
       </div>
     </div>
 
-    <div class="container" style="padding-top: 88px; padding-bottom: 184px;">
-      <div class="col-sm-12">
-        <div class="sponsors_slider">
-          <ul>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-            <li><img src="assets/image 2.png" alt="..."></li>
-          </ul>
-        </div>
+    <div style="padding-top: 88px; padding-bottom: 184px;">
+
+      <div class="sponsors_slider">
+        <ul>
+          <li><img src="./assets/img-slideshow/022-08-13 123144.jpg"></li>
+          <li><img src="./assets/img-slideshow/IMG_5546-min.jpg"></li>
+          <li><img src="./assets/img-slideshow/IMG_5605-min.jpg"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (1445)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (7)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (719)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (812)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (867)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (893)-min.png"></li>
+          <li><img src="./assets/img-slideshow/Screenshot (894)-min.png"></li>
+        </ul>
       </div>
+      <img class="image1" src="assets/image 1.png" width="100%">
+    </div>
   </section>
 
-  <section class="theme">
-
+  <section class="grand-theme container-fluid m-0 p-0">
+    <div class="grand-theme-width-limiter">
+      <div class="row mb-4">
+        <div class="col-5">
+          <!-- empty div for layouting purpose -->
+        </div>
+        <h3 class="col-7 pe-4">
+          Sovereignty and Commitment: Overcoming Post-pandemic Obstacles and Challenges
+        </h3>
+      </div>
     </div>
-    <?php require "./components/script.php" ?>
+    <div class="grand-theme-content px-5 py-4">
+      <div class="row grand-theme-width-limiter">
+        <div class="col-5 ">
+          <img src="" alt="img">
+        </div>
+        <p class="col-7">We are facing a new era after the COVID-19 pandemic, which has disrupted many aspects of life. As is well acknowledged, the COVID-19 outbreak has caused a severe global health crisis with devastating effects on the economy, society, and psychological health. Even though there have been many crises recently, including diseases, economic decline, and natural disasters, the coronavirus pandemic is the first to have a significant global impact in a century. If we look at the current condition after the pandemic, plenty of severe global issues are overtaking this world. Food crisis, monetary crisis, climate change, and the Russia-Ukraine issue are only a few examples of world hurdles that arise after this post-pandemic era. Therefore, we urge the sovereignty and commitment of the entire nation to overcome the post-pandemic obstacles and challenges so the world goals can be achieved sustainably.</p>
+      </div>
+    </div>
+  </section>
+  <?php require "./components/script.php" ?>
 </body>
 
 </html>
--- js/index.js
+++ js/index.js
@@ -10,12 +10,30 @@ window.onscroll = function () {
   prevScrollpos = currentScrollPos;
 };
 
-var scene = document.getElementById("scene");
-var parallax = new Parallax(scene);
+// COUNTDOWN
+var countDownDate = new Date("Jan 5, 2024").getTime();
 
-$(document).ready(function () {
-  $(".navbar-nav .dropdown-item").click(function () {
-    $(".navbar-nav .dropdown-item").removeClass("active");
-    $(this).addClass("active");
-  });
-});
+// Update the count down every 1 second
+var x = setInterval(function () {
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  // Display the result in the corresponding element
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+  }
+}, 1000);
