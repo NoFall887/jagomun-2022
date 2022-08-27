4fdf7ab3d67422e1d47a06a78f85fed7e34099f2
NoFall887 <naufalamiruddin123456@gmail.com>
2022-08-27 20:22:41 +0700

    change council page name

--- councils.php
+++ councils.php
@@ -11,7 +11,7 @@
   <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600&display=swap" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="./css/style.css">
-  <title>Jagomun 2022-Councils</title>
+  <title>Councils - Jagomun 2022</title>
 </head>
 
 <body>
