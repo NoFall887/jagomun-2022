1afc1eaf52be32d2d4917ec288b52e5e833d6793
TheodorDani <63799480+TheodorDani@users.noreply.github.com>
2022-10-02 22:18:26 +0700

    store page

---
+++ store.php
@@ -0,0 +1,135 @@
+<!doctype html>
+<html lang="en" id="home">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Jember Annual Global Model United Nations (JAGOMUN) is an event carried out by UKM UNEJ MUN CLUB. This is an international event which aims to serve as a forum for all young people as future world leadership candidates in training their views and thoughts on issues that occur in the international world from various perspectives of countries in the world.">
+  <!-- Bootstrap CSS -->
+
+  <link rel="icon" type="image/x-icon" href="/assets/favicon.ico">
+  <link href="https://fonts.googleapis.com/css2?family=EB+Garamond:wght@400;500;600&display=swap" rel="stylesheet">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+  <link rel="stylesheet" href="./css/style.css">
+  <title>Jagomun 2022</title>
+</head>
+
+<body class="aboutus-container">
+  <?php require "./components/navbar.php"; ?>
+
+  <section class="aboutus">
+    <h1 class="h1-blue col-md-6 mx-auto text-center grand-theme-width-limiter px-4">STORE</h1>
+  </section>  
+
+  <div class="book-slide" style="padding-top: 48px; padding-bottom: 48px;">
+  <div class="book js-flickity" data-flickity-options='{ "wrapAround": true }'>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/keychain.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">Keychain</h4>
+     <p class="book-author">IDR 10.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/munshirt.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">T-Shirt</h4>
+     <p class="book-author">IDR 80.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/diploshirt.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">T-Shirt</h4>
+     <p class="book-author">IDR 80.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/landyard.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">Lanyard</h4>
+     <p class="book-author">IDR 15.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/totebag1.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">Totebag</h4>
+     <p class="book-author">IDR 35.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/totebag2.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">Totebag</h4>
+     <p class="book-author">IDR 35.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+   <div class="book-cell d-flex justify-content-between">
+    <div class="book-img">
+     <img src="./assets/sticker.png" alt="" class="book-photo">
+    </div>
+    <div class="book-content d-flex flex-column">
+     <h4 class="book-title">Sticker</h4>
+     <p class="book-author">IDR 2.000</p>
+     <div class="book-see mt-auto">Order Now</div>
+    </div>
+   </div>
+  </div>
+</div>
+
+  <footer class="d-flex justify-content-between py-5 px-5 align-items-center home-footer">
+    <div>
+      <div class="mb-3">
+        <img src="./assets/LOGO MUN 2022 1.png" alt="logo" class="footer-jagomun-logo">
+      </div>
+      <p>
+        Jember Annual Global Model United Nations (JAGOMUN) is an event carried out by UKM UNEJ MUN CLUB. This is an international event which aims to serve as a forum for all young people as future world leadership candidates in training their views and thoughts on issues that occur in the international world from various perspectives of countries in the world.
+      </p>
+      <div style="height: 1px; background-color: rgba(8, 18, 23, 0.4)" class="mb-2"></div>
+      <span>
+        2022 All rights reserved
+      </span>
+    </div>
+    <div class="footer-contact">
+      <h6>
+        Contact Us :
+      </h6>
+      <a href="https://www.linkedin.com/company/jagomun/">
+        <img src="./assets/linkedin.svg" alt="linkedin logo">
+        jagomun
+      </a>
+      <a href="https://www.instagram.com/jagomun.2022/">
+        <img src="./assets/fa-brands_instagram-square.svg" alt="instagram logo">
+        jagomun.2022
+      </a>
+    </div>
+  </footer>
+
+  <?php require "./components/script.php" ?>
+</body>
+
+<!-- script -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.pkgd.js"></script> 
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/flickity/1.0.0/flickity.css"></script>
+</html>
