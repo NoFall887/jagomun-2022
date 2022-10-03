61ee8ec7571f4f327530ee61297354304f7384bf
TheodorDani <63799480+TheodorDani@users.noreply.github.com>
2022-10-03 19:35:24 +0700

    final store edit

--- store.php
+++ store.php
@@ -18,84 +18,99 @@
 <body class="aboutus-container">
   <?php require "./components/navbar.php"; ?>
 
-  <section class="aboutus">
-    <h1 class="h1-blue col-md-6 mx-auto text-center grand-theme-width-limiter px-4">STORE</h1>
-  </section>  
-
+<section class="aboutus">
+  <h1 class="h1-blue col-md-6 mx-auto text-center grand-theme-width-limiter px-4">STORE</h1>
   <div class="book-slide" style="padding-top: 48px; padding-bottom: 48px;">
-  <div class="book js-flickity" data-flickity-options='{ "wrapAround": true }'>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/keychain.png" alt="" class="book-photo">
+    <div class="book js-flickity" data-flickity-options='{ "wrapAround": true }'>
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/keychain.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">Keychain</h4>
+      <p class="book-author">IDR 10.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/keychain-jagomun-2022-x-achiera-inivitative-putih?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">Keychain</h4>
-     <p class="book-author">IDR 10.000</p>
-     <div class="book-see mt-auto">Order Now</div>
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/munshirt.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">T-Shirt</h4>
+      <p class="book-author">IDR 80.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/t-shirt-jagomun-2022-x-achiera-inivitative-d1-tosca-xs?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/munshirt.png" alt="" class="book-photo">
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/diploshirt.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">T-Shirt</h4>
+      <p class="book-author">IDR 80.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/t-shirt-jagomun-2022-x-achiera-inivitative-d1-tosca-xs?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">T-Shirt</h4>
-     <p class="book-author">IDR 80.000</p>
-     <div class="book-see mt-auto">Order Now</div>
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/landyard.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">Lanyard</h4>
+      <p class="book-author">IDR 15.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/lanyard-jagomun-2022-x-achiera-inivitative?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/diploshirt.png" alt="" class="book-photo">
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/totebag1.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">Totebag</h4>
+      <p class="book-author">IDR 35.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/totebag-jagomun-2022-x-achiera-inivitative-hitam?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">T-Shirt</h4>
-     <p class="book-author">IDR 80.000</p>
-     <div class="book-see mt-auto">Order Now</div>
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/totebag2.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">Totebag</h4>
+      <p class="book-author">IDR 35.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/totebag-jagomun-2022-x-achiera-inivitative-hitam?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/landyard.png" alt="" class="book-photo">
+    <div class="book-cell d-flex justify-content-between">
+      <div class="book-img">
+      <img src="./assets/sticker.png" alt="" class="book-photo">
+      </div>
+      <div class="book-content d-flex flex-column">
+      <h4 class="book-title">Sticker</h4>
+      <p class="book-author">IDR 2.000</p>
+      <div class="book-see mt-auto"><a href="https://www.tokopedia.com/achierain/set-sticker-jagomun-2022-x-achiera-inivitative?extParam=whid%3D12071860">Order Now</a></div>
+      </div>
     </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">Lanyard</h4>
-     <p class="book-author">IDR 15.000</p>
-     <div class="book-see mt-auto">Order Now</div>
     </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/totebag1.png" alt="" class="book-photo">
-    </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">Totebag</h4>
-     <p class="book-author">IDR 35.000</p>
-     <div class="book-see mt-auto">Order Now</div>
-    </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/totebag2.png" alt="" class="book-photo">
-    </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">Totebag</h4>
-     <p class="book-author">IDR 35.000</p>
-     <div class="book-see mt-auto">Order Now</div>
-    </div>
-   </div>
-   <div class="book-cell d-flex justify-content-between">
-    <div class="book-img">
-     <img src="./assets/sticker.png" alt="" class="book-photo">
-    </div>
-    <div class="book-content d-flex flex-column">
-     <h4 class="book-title">Sticker</h4>
-     <p class="book-author">IDR 2.000</p>
-     <div class="book-see mt-auto">Order Now</div>
-    </div>
-   </div>
   </div>
-</div>
+</section>  
+
+<section class="productdesc"style="padding-bottom: 184px;">
+    <div class="container-fluid px-5">
+      <div class="row d-flex align-items-center">
+        <div>
+          <h3 class="mb-5 text-center">How to Order</h3>
+          <p>
+          1. Click on the link <a href="https://linktr.ee/AchieraInivitative">https://linktr.ee/AchieraInivitative</a> (include more info about products and <a href="https://www.tokopedia.com/achierain/bundle-merchandise-jagomun-2022-x-achiera-inivitative-bundle-becik?extParam=whid%3D12071860">bundling package</a>)<br><br>
+          2. Click “CP for Snacks &  Merchandise Order” if you want to order manually by contacting our contact person<br><br>
+          3. Click “Shopee Achiera Inivitative 2022” to order from Shopee or “Tokopedia Achiera Inivitative 2022” to order from Tokopedia<br><br>
+          4. Choose any items that you liked and click on the cart logo to check out the items that you want to buy
+          </p>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <footer class="d-flex justify-content-between py-5 px-5 align-items-center home-footer">
     <div>
